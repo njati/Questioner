01356c531a44e9f5a140e87a02dc1c964b6dd2a0
njati <willien43@gmail.com>
2019-01-29 08:41:37 +0300

    [starts #163072038] edited the view meetup endpoint

--- app/api/v1/views/viewmeetups.py
+++ app/api/v1/views/viewmeetups.py
@@ -16,9 +16,9 @@ def meetups():
     meetup_data = {
         "meetup_id": len(MEETUP_DETAILS.meetups_database)+1,
         "created_at": datetime.datetime.now(),
-        "meetup_date":data["meetup_date"],
-        "topic":data["topic"],
-        "about":data["about"],
+        "meetup_date": data["meetup_date"],
+        "topic": data["topic"],
+        "about": data["about"],
         "location": data["location"],
         "meetup_image":data["meetup_image"]
 
